bfb9febfcee0b93cc6013d1785f837cd44d57aa3
大猫 <tiger@skson.com>
2019-12-15 20:07:38 +0800

    dddddddddddddddddddd

--- src/main/java/com/skson/wocao/WocaoApplication.java
+++ src/main/java/com/skson/wocao/WocaoApplication.java
@@ -1,15 +1,19 @@
 package com.skson.wocao;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WocaoApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(WocaoApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(WocaoApplication.class, args);
 		for (int i = 0; i <100 ; i++) {
 			System.out.println("=======================================");
+			logger.info("--------------9909890");
 		}
 	}
 
