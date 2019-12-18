02369aee9194d21e9e42d506bce6b265ba566b0c
大猫 <tiger@skson.com>
2019-12-18 22:48:13 +0800

    更改打包jar到war

--- src/main/java/com/skson/wocao/WocaoApplication.java
+++ src/main/java/com/skson/wocao/WocaoApplication.java
@@ -4,18 +4,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class WocaoApplication {
+public class WocaoApplication extends SpringBootServletInitializer {
 
 	private static Logger logger = LoggerFactory.getLogger(WocaoApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(WocaoApplication.class, args);
 		System.out.println("=======================================");
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WocaoApplication.class);
 	}
 
 	@RequestMapping("/")
